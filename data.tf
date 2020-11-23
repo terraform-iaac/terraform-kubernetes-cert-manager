0f8456a2249a1d3f06e4bdb13d55fe99c902bbec
Bohdan Tverdyi <ownedd777@gmail.com>
2020-11-23 14:35:41 +0200

    Add outputs

--- data.tf
+++ data.tf
@@ -4,6 +4,7 @@ data "template_file" "cluster_issuer" {
   vars = {
     email                   = var.cluster_issuer_email
     server                  = var.cluster_issuer_server
+    namespace               = kubernetes_namespace.cert_manager.id
     name                    = var.cluster_issuer_name
     private_key_secret_name = var.cluster_issuer_private_key_secret_name
   }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+output "namespace" {
+  value = kubernetes_namespace.cert_manager.id
+}
 output "cluster_issuer_name" {
   value = var.cluster_issuer_name
+}
+output "cluster_issuer_server" {
+  value = var.cluster_issuer_server
+}
+output "cluster_issuer_private_key_name" {
+  value = var.cluster_issuer_private_key_secret_name
 }
